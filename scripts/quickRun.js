e81b989871314b8c4c304e13788eee1d32327c7e
Matthew Vitt <vittmatt1994@gmail.com>
2021-12-10 19:00:18 -0600

    cleanup

--- scripts/quickRun.js
+++ scripts/quickRun.js
@@ -14,13 +14,14 @@ const overrides = {
     value: ethers.utils.parseEther('0.000000000000000100')
 }
 
-// var sendPromise = bettingContract.requestTemperatureFor("0xea435393E8EA5d007F838CE9fd5b6Fa2c66A5A8e", "97bffeec72a24c4f9d29d36e9662ed3b", "Chicago", 1);
-//var sendPromise = bettingContract.temperaturePrediction();
-var sendPromise = bettingContract.placeBet(1, overrides);
+//var sendPromise = bettingContract.requestTemperatureFor("0xea435393E8EA5d007F838CE9fd5b6Fa2c66A5A8e", "dce7c3107f314c61a0c70499bb9a1709", "Chicago", 1);
+// var sendPromise = bettingContract.temperaturePrediction();
+var sendPromise = bettingContract.actualTemperature();
+// var sendPromise = bettingContract.placeBet(1, overrides);
 // var sendPromise = bettingContract.getBet("0x014Da1D627E6ceB555975F09D26B048644382Ac6");
 
 
 sendPromise.then(function(transaction){
-  console.log(transaction);
-  //console.log(ethers.BigNumber.from(transaction).toNumber()); //For converting hex of temp to a number
+//   console.log(transaction);
+  console.log(ethers.BigNumber.from(transaction).toNumber()); //For converting hex of temp to a number
 });
